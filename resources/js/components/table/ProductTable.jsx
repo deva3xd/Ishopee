60cda26af6aca124d25f74f11b3182eec97e5904
deva3xd <devarn05@gmail.com>
2025-11-13 20:57:47 +0700

    change directory name

---
+++ resources/js/components/table/ProductTable.jsx
@@ -0,0 +1,48 @@
+import { Link } from "@inertiajs/react";
+
+const ProductTable = ({ products }) => {
+  return (
+    <div>
+      <div className="overflow-x-auto rounded-sm border border-base-content/5 bg-base-100 mt-4 mb-2">
+        <div className="p-4 font-medium text-lg">PRODUCT TABLE</div>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Category</th>
+              <th>Seller</th>
+              <th>Price ($)</th>
+            </tr>
+          </thead>
+          <tbody>
+            {products.data.map((product) => (
+              <tr key={product.id}>
+                <th>{product.id}</th>
+                <td>{product.name}</td>
+                <td>{product.category.name}</td>
+                <td>{product.profile.name}</td>
+                <td>{product.price}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div className="flex justify-between mt-2">
+        <div>Showing {products.meta.from} to {products.meta.to} of {products.meta.total} entries</div>
+        <div className="join">
+          {products.meta.links.map((product) => (
+            <Link
+              key={product?.label ?? "#"}
+              href={product?.url ?? "#"}
+              className={`join-item btn ${product.active === true && "btn-active"}`}
+              dangerouslySetInnerHTML={{ __html: product.label }}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ProductTable;
